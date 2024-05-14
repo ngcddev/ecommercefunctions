b2166201d73d751155ddbe766fd82462d3709948
viveros546 <viverosgeranios@gmail.com>
2024-05-14 07:22:44 -0500

    actualizacion

--- login.php
+++ login.php
@@ -1,21 +1,17 @@
 <?php
 session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST['username']; $password = $_POST['password'];
 
-$_SESSION['username'] =$username ;
-$_SESSION['password'] =$password;
+$_SESSION['username'] =$username ; $_SESSION['password'] =$password;
 
 $conexion = mysqli_connect("localhost", "seccion", "3157657766", "inventory_db");
 
 
-$username = mysqli_real_escape_string($conexion, $username);
-$password = mysqli_real_escape_string($conexion, $password );
+$username = mysqli_real_escape_string($conexion, $username); $password = mysqli_real_escape_string($conexion, $password );
 
 $consulta = "SELECT username, passworduser FROM users WHERE username='$username' AND passworduser='$password '";
-$resultado = mysqli_query($conexion, $consulta);
-$filasadmin = mysqli_num_rows($resultado);
+$resultado = mysqli_query($conexion, $consulta); $filasadmin = mysqli_num_rows($resultado);
 
 if ($filasadmin > 0) {
     $filasadmin= mysqli_fetch_assoc($resultado);
--- update_inventory.php
+++ update_inventory.php
@@ -1,18 +1,12 @@
 <?php
 session_start();
 
-
 if (!isset($_SESSION['username'])) {
     header("Location: index.php"); 
     exit();
 }
 
-
-$servername = "localhost";
-$username = "seccion";
-$password = "3157657766"; 
-$dbname = "inventory_db";
-
+$servername = "localhost"; $username = "seccion"; $password = "3157657766";  $dbname = "inventory_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -20,17 +14,21 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-
-$product_name = $_POST['product_name'];
-$category = $_POST['category'];
-$description = $_POST['description'];
-$update_time = date("Y-m-d H:i:s"); // Fecha y hora actuales
+$product_name = $_POST['product_name']; $category = $_POST['category']; $description = $_POST['description']; $update_time = date("Y-m-d H:i:s"); 
 
 
-$sql = "INSERT INTO inventory_updates (product_name, category, description, update_time) VALUES ('$product_name', '$category', '$description', '$update_time')";
+$image_name = $_FILES['image']['name']; $image_tmp = $_FILES['image']['tmp_name'];
+
+$image_name = mysqli_real_escape_string($conn, $image_name);
+
+$upload_dir = "uploads/"; $image_path = $upload_dir . basename($image_name); move_uploaded_file($image_tmp, $image_path);
+
+
+$sql = "INSERT INTO inventory_updates (product_name, category, description, update_time, images) VALUES ('$product_name', '$category', '$description', '$update_time', '$image_path')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Inventario actualizado con éxito.";
+    header("Location: inventory_form.php"); 
 } else {
     echo "Error al actualizar el inventario: " . $conn->error;
 }
